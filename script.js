2d536f45b2fac781f7340c9664378d823abbec85
MAHDIA001 <mahdianaiebi@gmail.com>
2022-09-14 22:58:18 +0500

    fix linters error

--- script.js
+++ script.js
@@ -1,66 +1,65 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle("active");
-   menu.classList.toggle("active");
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
 });
 
-document.querySelectorAll('.menu').forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    menu.classList.remove("active");
+document.querySelectorAll('.menu').forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menu.classList.remove('active');
   })
 );
 
 // featured section
 const speaker = [
   {
-    image: "images/p.jpg",
-    name: "Mahdia Naiebi",
-    Job: "Director of Google student club and",
+    image: 'images/p.jpg',
+    name: 'Mahdia Naiebi',
+    Job: 'Director of Google student club and',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias',
   },
   {
-    image: "images/speaker.jpg",
-    name: "john Smith",
-    Job: "Director of Google student club and",
+    image: 'images/speaker.jpg',
+    name: 'john Smith',
+    Job: 'Director of Google student club and',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias',
   },
   {
-    image: "images/speaker.jpg",
-    name: "Millad Qurban",
-    Job: "Director of Google student club and",
+    image: 'images/speaker.jpg',
+    name: 'Millad Qurban',
+    Job: 'Director of Google student club and',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias',
   },
   {
-    image: "images/p.jpg",
-    name: "zuhal Hameed",
-    Job: "Director of Google student club and",
+    image: 'images/p.jpg',
+    name: 'zuhal Hameed',
+    Job: 'Director of Google student club and',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias',
   },
   {
-    image: "images/speaker.jpg",
-    name: "Mariam",
-    Job: "Director of Google student club and",
+    image: 'images/speaker.jpg',
+    name: 'Mariam',
+    Job: 'Director of Google student club and',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias',
   },
   {
-    image: "images/p.jpg",
-    name: "Mahdia Naieb",
-    Job: "Director of Google student club and",
+    image: 'images/p.jpg',
+    name: 'Mahdia Naieb',
+    Job: 'Director of Google student club and',
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias",
-  }
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto harum neque ipsa vero alias',
+  },
 ];
 
 const featured = document.querySelector('.featured');
-for (let i=0;i<speaker.length;i+=1){
-featured.innerHTML = `
+for (let i = 0; i < speaker.length; i+=1) {
+  featured.innerHTML = `
  <div class="mobile-sectio">
         <div class="contains">
         <div class="speaker">
